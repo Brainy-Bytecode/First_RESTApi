9241431759a7003656bd818085ec2729668cdbdc
Sushmita Singh <sushmitasachan97@gmail.com>
2023-01-27 22:32:16 +0530

    Initial Commit - Added Layered view

---
+++ src/main/java/com/firstproject/studentManagement/StudentManagementApplication.java
@@ -0,0 +1,17 @@
+package com.firstproject.studentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+}
