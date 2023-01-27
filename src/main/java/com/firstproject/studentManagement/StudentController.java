1a4f55e362b57a58e1000b4ad7c81d07ba798d2d
Sushmita Singh <sushmitasachan97@gmail.com>
2023-01-28 03:21:52 +0530

    added <> after ResposeEntitiy>

--- src/main/java/com/firstproject/studentManagement/StudentController.java
+++ src/main/java/com/firstproject/studentManagement/StudentController.java
@@ -15,20 +15,20 @@ public class StudentController {
     StudentService studentService;
 
     @GetMapping("/get_student")
-    public ResponseEntity getStudent(@RequestParam("admnNo") int admnNo){
+    public ResponseEntity<Student> getStudent(@RequestParam("admnNo") int admnNo){
         Student student = studentService.getStudent(admnNo);
         return new ResponseEntity<>(student, HttpStatus.FOUND);
     }
     // adding the information
     @PostMapping("/add_student")
-    public ResponseEntity addStudent(@RequestBody Student student){
+    public ResponseEntity<String> addStudent(@RequestBody Student student){
 
         String response = studentService.addStudent(student);
         return new ResponseEntity<>(response,HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete_student/{id}")
-    public ResponseEntity deleteStudent(@PathVariable("id") int id){
+    public ResponseEntity<String> deleteStudent(@PathVariable("id") int id){
         String response = studentService.delete_Student(id);
         if(response.equals("Invalid id")){
             return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
@@ -37,12 +37,12 @@ public class StudentController {
         return new ResponseEntity<>(response,HttpStatus.FOUND);
     }
     @PutMapping("/update_student")
-    public ResponseEntity updateStudent(@RequestParam("id") int id,@RequestParam("age") int age){
+    public ResponseEntity<String> updateStudent(@RequestParam("id") int id,@RequestParam("age") int age){
         String response = studentService.updateStudent(id,age);
         if(response==null){
-            return new ResponseEntity("Invalid request",HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("Invalid request",HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity("Updated",HttpStatus.ACCEPTED);
+        return new ResponseEntity<>("Updated",HttpStatus.ACCEPTED);
     }
 
 
